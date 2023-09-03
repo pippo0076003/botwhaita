e15698199f91e6e5536a41c37801a840f505fbd9
pippo0076003 <143545753+pippo0076003@users.noreply.github.com>
2023-09-03 12:06:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,7 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['390371717717', 'nobody|'],
-  ['393755775755', 'nobody|'],
-  ['393755575577', 'nobody|'],
-  ['33760536110', 'nobody|'],
+  ['393293642655', 'nobody|'],
 ]
 global.suittag = ['390371717717'] 
 global.prems = ['390371717717'] 
